81c205493eb512d9cd15d3f221da3e5088342435
JK0369 <palatable7@naver.com>
2020-05-01 02:37:13 +0900

    slide out view

---
+++ TmpTestCGAffine/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  TmpTestCGAffine
+//
+//  Created by 김종권 on 2020/05/01.
+//  Copyright © 2020 mustang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var btn: UIButton!
+    var vc: ViewController2!
+    var width: CGFloat!
+    var isOpen: Bool!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+        
+        isOpen = false
+        
+        btn = UIButton()
+        btn.setTitle("slide btn", for: .normal)
+        btn.backgroundColor = .black
+        btn.frame = CGRect(x: 50, y: 30, width: 100, height: 30)
+        view.addSubview(btn)
+        
+        vc = ViewController2()
+        vc.view.backgroundColor = .blue
+        width = view.frame.width
+        let height = view.frame.height
+        
+        vc.view.frame = CGRect(x: -width, y: 0, width: width, height: height)
+        view.addSubview(vc.view)
+        
+        btn.addTarget(self, action: #selector(toggle(_:)), for: .touchUpInside)
+    }
+
+    @objc func toggle(_ sender: Any) {
+        if isOpen == false {
+            isOpen = true
+            view.transform = CGAffineTransform(translationX: 50, y: 0)
+            vc.view.transform = CGAffineTransform(translationX: 50, y: 0)
+        } else {
+            isOpen = false
+            view.transform = CGAffineTransform(translationX: 0, y: 0)
+            vc.view.transform = CGAffineTransform(translationX: -50, y: 0)
+        }
+    }
+
+}
---
+++ TmpTestCGAffine/ViewController2.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController2.swift
+//  TmpTestCGAffine
+//
+//  Created by 김종권 on 2020/05/01.
+//  Copyright © 2020 mustang. All rights reserved.
+//
+
+import UIKit
+class ViewController2: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
